289170778cd8f1ddb8f9bb8154d034af19dc6c03
jaishreeOU <jaishree.raman-1@ou.edu>
2023-04-13 16:09:24 -0500

    jralumniproject0412 original main

--- jralumniarchive/views.py
+++ jralumniarchive/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+# from django.utils import timezone
+from .models import Student
 
 # Create your views here.
-#try
+# try
+
+
+def student_list(request):
+    students = Student.objects.order_by('created_date')
+#        #filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'viewshtmls/student_list.html', {'students': students})
